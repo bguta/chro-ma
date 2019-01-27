37ada282dc36db0237b0225e1dec8280ce5cc766
tomginsberg <tommyginsberg@gmail.com>
2019-01-27 11:11:29 -0800

    progress bar for recording

--- record.py
+++ record.py
@@ -4,6 +4,27 @@ import matplotlib.pyplot as plt
 import time
 
 
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
 def rec():
     filename = 'data/output.avi'
     vid = cv2.VideoCapture(0)
@@ -27,6 +48,8 @@ def rec():
     print("Starting ... ")
     out = cv2.VideoWriter(filename, fourcc, 30.0, (640, 480))
     for i in range(samples + 15):
+        printProgressBar(i, samples+15, prefix='Progress:', suffix='Complete', length=50)
+
         ret, img = vid.read()
         if (i > 15):
             out.write(img)
