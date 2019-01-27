900a6c7d5ec12efe22724089a3c050162311c52d
tomginsberg <tommyginsberg@gmail.com>
2019-01-26 18:10:54 -0800

    uncommented, testing fft

--- cambox.py
+++ cambox.py
@@ -6,29 +6,29 @@ import matplotlib.pyplot as plt
 def main():
     face_c = cv2.CascadeClassifier(
         "classifier/haarcascade_frontalface_default.xml")
-    vid = cv2.VideoCapture('vic1.mov')
+    vid = cv2.VideoCapture(0)
 
-    x, y, w, h = 300, 300, 100, 100
+    x, y, w, h = 600, 100, 100, 100
 
-    samples = 500
+    samples = 200
     avg = np.zeros(samples)
 
     ready = False
 
-    # print("Waiting ...")
-    # print("Enter q to start")
-    # while not ready:
-    #     ret, img = vid.read()
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
-    #     cv2.imshow('vid', img)
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         break
+    print("Waiting ...")
+    print("Enter q to start")
+    while not ready:
+        ret, img = vid.read()
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+        cv2.imshow('vid', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     print("Starting ... ")
     for i in range(samples):
         ret, img = vid.read()
         face = img[y:(y + h), x:(x + w)]
-        avg[i] = np.sum(face) / (w * h)
+        avg[i] = np.sum(face) / (3* w * h)
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
         cv2.imshow('vid', img)
